827a50f7248c8c76619d8f44cfc2e17cb9880690
CarmenLeila <carmenmugisha18@gmail.com>
2023-10-14 07:48:47 -0700

    5. Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Program that using a printf function
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar,\but the outcome is a piece of art,\");	
+	    return (0);
+}
