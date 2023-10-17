0cbae3597b6ac1eedc969e22159dbdd63d1335a2
CarmenLeila <carmenmugisha18@gmail.com>
2023-10-17 04:41:36 -0700

    program that prints the alphabet in lowercase

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -Program that prints the alphabet in lowercase
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+	 printf("_putchar\n");
+	   return 0;
+}
