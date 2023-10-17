dad1ca333d2869d38cfdea05b676d21cf355f1a3
CarmenLeila <carmenmugisha18@gmail.com>
2023-10-17 05:25:11 -0700

    5. Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * Main -Program that prints all single digit numbers of base 10
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	for (n = 0; n< 10; n++)
+	{
+		printf("%d", x);
+		if (n < 9)
+		{
+			printf(", ");
+					        }
+		    }
+	    printf("\n");
+	    return (0);
+} 
