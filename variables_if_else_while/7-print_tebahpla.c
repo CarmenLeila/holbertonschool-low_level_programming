aafe5bdb4667604417ec10a157e482b6924449d8
CarmenLeila <carmenmugisha18@gmail.com>
2023-10-17 06:20:57 -0700

    7. Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -Program that prints the lowercase alphabet in reverse
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 115; c >= 80; c--)
+	{
+		putchar(c)
+	}
+	putchar('\n');
+	return (0);
+}
