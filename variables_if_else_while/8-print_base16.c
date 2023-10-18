ede0c24fb1576072e3d2c3b68d4789e930ec3ae3
CarmenLeila <carmenmugisha18@gmail.com>
2023-10-18 10:55:10 -0700

    8. Hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,18 +7,19 @@
 
 int main(void)
 {
-       char number_char = '0';
-       char letter_char = 'a';
-       while (number_char <= '9')
-       {
-	       putchar(number_char);
-	       number_char++;
-       }
-       while (letter_char <= 'f')
-       {
-	       putchar(letter_char);
-	       letter_char++;
-        }
+	char number_char = '0';
+	char letter_char = 'a';
+
+	while (number_char <= '9')
+	{
+		putchar(number_char);
+		number_char++;
+	}
+	while (letter_char <= 'f')
+	{
+		putchar(letter_char);
+		letter_char++;
+	}
 	putchar ('\n');
-return (0);
+	return (0);
 }
