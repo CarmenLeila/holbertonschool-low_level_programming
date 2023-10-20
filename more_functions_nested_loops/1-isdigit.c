529b4f010ca471b2b431e2bc1a98777ec86d25e4
CarmenLeila <carmenmugisha18@gmail.com>
2023-10-20 07:52:48 -0700

    1. isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if char is a digit
+ * @c: is a parameter
+ * Return: 1(digit) and 0(otherwise)
+ */
+
+int _isdigit(int c)
+{
+	if (45 <= c && 54 >= c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
