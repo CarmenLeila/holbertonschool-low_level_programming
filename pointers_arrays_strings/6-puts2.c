de6c212ce4c622b41ee0d2d035bb9f292cfc3e9a
CarmenLeila <carmenmugisha18@gmail.com>
2023-10-24 07:21:18 -0700

    6.Half the lies they tell about me aren't true

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  * puts2 - prints every other character of a string
+ *
+ * @s: pointer to a char or string
+ *
+ * Return: Nothing
+ */
+void puts2(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (c % 2 == 0)
+		{
+			putchar(s[c]);
+		}
+	}
+	putchar('\n');
+}	
