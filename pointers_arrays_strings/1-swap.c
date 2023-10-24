2370ba6550ae54c03aa891ba09a006298a8dc004
CarmenLeila <carmenmugisha18@gmail.com>
2023-10-24 01:21:14 -0700

    1.swaps the values of two integers

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * swap_int -  swaps the values of two integers
+ * swap_int - swaps the values of two integers
  *
  * @a: first integer
  *
@@ -11,13 +11,11 @@
  * Return: Nothing
  */
 
-void swap_int(int a, int b)
+void swap_int(int *a, int *b)
 {
 	int res;
-	res= a;
-	a = 98;
-	b = 42;
-	res = b;
 
-	putchar("a = %d && b = %d\n", a, b);
-}	
+	res = *a;
+	*a = *b;
+	*b = res;
+}
