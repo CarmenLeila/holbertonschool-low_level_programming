a364396ea60e88f196182aa1fd9825831c9a9d5c
CarmenLeila <carmenmugisha18@gmail.com>
2023-10-29 10:37:44 -0700

    0. memset

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int res = 0;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		res = a[i];
+		a[i] = *(a + n - 1 - i);
+		*(a + n - 1 - i) = res;
+	}
+}
---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string
+ *
+ * @str: string
+ *
+ * Return: a string
+ */
+char *leet(char *)
+{
+	int i = 0;
+	int x = 0;
+
+	char ch[] = {'a','e','o','t','l'};
+	char rep_ch[] = {'4', '3', '0', '7', '1'};
+	while (*(str + i))
+	{
+		for (x = 0; x < 5; x++)
+		{
+			if ((*(str + i) == *(ch + x)) || (*(str + i) == (*(ch + x) - 'a' + 'A')))
+			{
+				*(str + i) = *(rep_ch + x);
+				break;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
