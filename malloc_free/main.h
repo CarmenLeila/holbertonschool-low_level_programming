c9310d36ee19b8627d0ef2a7bf5c159053a175ea
CarmenLeila <carmenmugisha18@gmail.com>
2023-11-07 12:10:00 -0800

    0. a function that creates an array of chars

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
 
 int _putchar(char);
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int, char);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
+
+#endif
