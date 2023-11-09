bcc3fac2a8dbf451c9b460d35d65ea032d3fb9a3
CarmenLeila <carmenmugisha18@gmail.com>
2023-11-09 12:23:34 -0800

    2.function that allocates memory for an array

--- malloc_free/3-main.c
+++ malloc_free/3-main.c
@@ -12,23 +12,23 @@
  */
 void print_grid(int **grid, int width, int height)
 {
-	int w;
-	int h;
-	
-	h = 0;
-	while (h < height)
-	{
-		w = 0;
+    int w;
+    int h;
 
-		while (w < width)
-		{
-			printf("%d ", grid[h][w]);
-			w++;
-		}
-		printf("\n");
-		        h++;
-	}
+    h = 0;
+    while (h < height)
+    {
+        w = 0;
+        while (w < width)
+        {
+            printf("%d ", grid[h][w]);
+            w++;
+        }
+        printf("\n");
+        h++;
+    }   
 }
+
 /**
  * main - check the code
  *
@@ -36,17 +36,17 @@ void print_grid(int **grid, int width, int height)
  */
 int main(void)
 {
-	int **grid;
-	
-	grid = alloc_grid(6, 4);
-	if (grid == NULL)
-	{
-		return (1);
-	}
-	print_grid(grid, 6, 4);
-	printf("\n");
-	grid[0][3] = 98;
-	grid[3][4] = 402;
-	print_grid(grid, 6, 4);
-	return (0);
+    int **grid;
+
+    grid = alloc_grid(6, 4);
+    if (grid == NULL)
+    {
+        return (1);
+    }
+    print_grid(grid, 6, 4);
+    printf("\n");
+    grid[0][3] = 98;
+    grid[3][4] = 402;
+    print_grid(grid, 6, 4);
+    return (0);
 }
