6668a0a3093f631c342dcd243e95c160dccb3f29
CarmenLeila <carmenmugisha18@gmail.com>
2023-11-10 07:21:43 -0800

    2.2. _calloc

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,7 +1,9 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -9,3 +11,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _putchar(char c);
 
+#endif
