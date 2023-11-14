ae5f7f1310e2ed86dc1c9e723093f2a7fec121a8
CarmenLeila <carmenmugisha18@gmail.com>
2023-11-14 03:09:13 -0800

    0. Poppy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,17 @@
-struct dog {
-    char *name;
-    float age;
-    char *owner;
-};
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * struct dog - new type
+ * @name: type char of name
+ * @age: type float of age
+ * @owner: type char of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
