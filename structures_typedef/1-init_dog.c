980f440ca11d9ea7e7a53a9e6c53a5edd63f24c1
CarmenLeila <carmenmugisha18@gmail.com>
2023-11-15 05:26:59 -0800

    1. function that initialize a variable of type struct dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,53 +1,51 @@
-#include <stdio.h>
 #include "dog.h"
-#include <stdlib.h>
 
 /**
- * init_dog - initialize a variable of type struct dog
- * @d: my first parameter
- * @name: my second parameter as character
- * @age: my third parameter as a float
- * @owner: an other parameter as character
+ * init_dog - function that initialize a dog structure
+ * @d: the dog structure
+ * @name: a dog name
+ * @age: an age of the dog
+ * @owner: the name of the dog owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	unsigned int i = 0;
+unsigned int i = 0;
 
-	while (*(name + i))
-	{
-		i++;
-	}
+while (*(name + i))
+{
+i++;
+}
 
-	d->name = malloc(sizeof(char) * (i + 1));
-	if (d->name == NULL)
-	{
-		return;
-	}
-	i = 0;
-	while (*(name + i))
-	{
-		*(d->name + i) = *(name + i);
-		i++;
-	}
-	*(d->name + i) = '\0';
+d->name = malloc(sizeof(char) * (i + 1));
+if (d->name == NULL)
+{
+return;
+}
+i = 0;
+while (*(name + i))
+{
+*(d->name + i) = *(name + i);
+i++;
+}
+*(d->name + i) = 0;
 
-	i = 0;
-	while (*(owner + i))
-	{
-		i++;
-	}
-	d->owner = malloc(sizeof(char) * (i + 1));
-	if (d->owner == NULL)
-	{
-		return;
-	}
-	i = 0;
-	while (*(owner + i))
-	{
-		*(d->owner + i) = *(owner + i);
-		i++;
-	}
-	*(d->owner + i) = '\0';
+i = 0;
+while (*(owner + i))
+{
+i++;
+}
+d->owner = malloc(sizeof(char) * (i + 1));
+if (d->owner == NULL)
+{
+return;
+}
+i = 0;
+while (*(owner + i))
+{
+*(d->owner + i) = *(owner + i);
+i++;
+}
+*(d->owner + i) = 0;
 
-	d->age = age;
+d->age = age;
 }
