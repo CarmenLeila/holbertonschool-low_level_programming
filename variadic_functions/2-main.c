d9037c99ae3db5f92b1a2a4504161a00247ecd8b
CarmenLeila <carmenmugisha18@gmail.com>
2023-11-17 06:48:09 -0800

    2.Fuction used in the program

---
+++ variadic_functions/2-main.c
@@ -0,0 +1,15 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_strings(", ", 2, "Jay", "Django");
+    return (0);
+}
