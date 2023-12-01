d259df5d50bdb28139a0d580fddcbe9e6ba139d7
CarmenLeila <carmenmugisha18@gmail.com>
2023-12-01 09:24:06 -0800

    0.function that converts a binary number to an unsigned int

---
+++ function_pointers/3-op_func.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addion function
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - subtract function
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - multiplication funct
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - division function
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
+}
+
+/**
+ * op_mod - modulo function
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
+}
