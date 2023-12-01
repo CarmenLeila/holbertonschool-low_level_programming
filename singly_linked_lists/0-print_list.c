25c2059a98f44a62137d4ce71e973207367bec14
CarmenLeila <carmenmugisha18@gmail.com>
2023-12-01 13:22:24 -0800

    0.Fixing a bug with the code

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "lists.h"
 
+size_t show_list(const list_t *, const list_t *);
+
 /**
  * print_list - function that prints all the elements of a list_t list
  * @h: is a linked list of type list_t to print
@@ -8,18 +10,38 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t number = 0;
+	size_t size = 0;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		number++;
-		h = h->next;
-	}
-	return (number);
+	if (h == NULL)
+		return (0);
+	size++;
+	if (h->str != NULL)
+		printf("[%d] %s\n", h->len, h->str);
+	else
+		printf("[0] (nil)\n");
+
+	if (h->next != NULL)
+		size += show_list(h->next, h);
+
+	return (size);
 }
+
+/**
+ * show_list - function that shows a linked list
+ * @head: head node
+ * @list: a list to print
+ * Return: nothing
+ */
+size_t show_list(const list_t *list, const list_t *head)
+{
+	if (list == NULL || list == head)
+		return (0);
+
+	if (list->str != NULL)
+		printf("[%d] %s\n", list->len, list->str);
+	else
+		printf("[0] (nil)\n");
+
+	return (1 + show_list(list->next, head));
+}
+
