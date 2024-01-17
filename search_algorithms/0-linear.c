11cef2708de1d26dcb6b8555abc9545b0649eaa5
CarmenLeila <carmenmugisha18@gmail.com>
2024-01-17 13:36:50 +0100

    0.fixing bug in code

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -14,17 +14,18 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	size_t index;
 
 	if (array == NULL)
 		return (-1);
 
-	while (i < size)
+	for (index = 0; index < size; index++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-	if (array[i] == value)
-		return (i);
-		i++;
+		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
 	}
+
 	return (-1);
 }
